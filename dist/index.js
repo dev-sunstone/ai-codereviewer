17e2b6bb81d726f24c8cad5258ac5a608e0d1a81
snyamshrma <sharmasanyam50@gmail.com>
2025-03-06 19:41:00 +0530

    added response log.,

--- dist/index.js
+++ dist/index.js
@@ -112,6 +112,7 @@ const generateAIResponse = (prompt, apiUrl, apiKey) => __awaiter(void 0, void 0,
     try {
         console.log("CONFIG", config)
         const response = yield axios_1.default.request(config);
+        console.log("RESPONSE", response)
         const usefulResponse = JSON.parse(response['response']);
         console.log("RESPONSE", usefulResponse)
         return usefulResponse.reviews;
